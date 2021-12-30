e6deafed6ec614fef4778b4870b6b2ce026331ef
melbinjojo <96787500+melbinjojo@users.noreply.github.com>
2021-12-30 14:01:03 +0530

    Add files via upload

---
+++ REVERSE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,sum=0,r,temp;
+	clrscr();
+	printf("Enter a number");
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		r=n%10;
+		sum=sum*10+r;
+		n=n/10;
+	}
+	printf("Reverse of %d is %d",temp,sum);
+	getch();
+}

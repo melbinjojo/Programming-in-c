73d24b5a71447c730cecb149c8989dd9b1cb9be6
melbinjojo <96787500+melbinjojo@users.noreply.github.com>
2022-02-04 22:42:34 +0530

    Create CALLOC.C

---
+++ CALLOC.C
@@ -0,0 +1,22 @@
+lines (22 sloc)  379 Bytes
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+	int n,*ptr,i;
+	clrscr();
+	printf("enter number of elements");
+	scanf("%d",&n);
+	ptr=(int*)calloc(n,sizeof(int));
+	printf("\nenter elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",ptr+i);
+	}
+	printf("\narray elements are \n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",*(ptr+i));
+	}
+	getch()
